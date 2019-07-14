3bc378a024723f6026a9045204d7fc75c81ffbc5
Gesen <gesen@zhihu.com>
2019-07-14 11:07:24 +0800

    Initial commit

---
+++ SwiftUI-WeChat/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeChat
+//
+//  Created by Gesen on 2019/7/14.
+//  Copyright © 2019 Gesen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
