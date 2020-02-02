0ae1ebe671dde9e8798bb9dbc60397dfeef53565
Gesen <i@gesen.me>
2020-02-02 11:44:36 +0800

    优化代码

--- SwiftUI-WeChat/Bootstrap/SceneDelegate.swift
+++ SwiftUI-WeChat/Bootstrap/SceneDelegate.swift
@@ -14,10 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
         configureAppearance()
