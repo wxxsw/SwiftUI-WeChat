d65a67c8d7f5d30bb3652fca71194b0f0ba666a7
Gesen <i@gesen.me>
2020-03-01 17:37:09 +0800

    docs(moment): 增加滚动时导航栏及状态栏动态变化的注释

--- SwiftUI-WeChat/Module/Moment/MomentView.swift
+++ SwiftUI-WeChat/Module/Moment/MomentView.swift
@@ -22,6 +22,7 @@ struct MomentView: View {
                 List {
                     Group {
                         Header()
+                            // 将 Header 的底部坐标变化传递给上层，用于导航栏变化
                             .anchorPreference(key: NavigationKey.self, value: .bottom) { [$0] }
                         
                         ForEach(self.moments) { moment in
@@ -52,11 +53,17 @@ struct MomentView: View {
         let progress: CGFloat
         
         if let anchor = value.first {
+            // proxy[anchor] 作用是得到 anchor 在 proxy 中的相对位置
+            // -proxy[anchor].y 为 0 时代表 Header 底部正好在界面顶部的位置
+            // 为了与导航栏高度配合，+ height + 20，过渡位置更缓和
+            // 最后 / 44 即在 44px 距离内完成隐藏到显示
             progress = max(0, min(1, (-proxy[anchor].y + height + 20) / 44))
         } else {
+            // 这种情况是 Header 完全不在界面中，一般也就是滑出屏幕外了
             progress = 1
         }
 
+        // 同时更新状态栏样式
         UIApplication.shared.setStatusBar(style: progress > 0.3 ? .default : .lightContent)
         
         return Navigation(progress: Double(progress))
