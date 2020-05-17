52f0f627c421b25c92677388641b12c74ddb461c
Gesen <i@gesen.me>
2020-05-17 18:50:53 +0800

    feat(chat): 增加底部样式以及气泡 Arrow

--- SwiftUI-WeChat/Module/Chat/ChatCell.swift
+++ SwiftUI-WeChat/Module/Chat/ChatCell.swift
@@ -13,9 +13,11 @@ struct ChatCell: View {
     let isMe: Bool
     
     var body: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .top, spacing: 8) {
             if isMe { Spacer() } else { Avatar(icon: message.member.icon) }
-            TextMessage(isMe: isMe, text: message.text ?? "")
+            
+            if message.type == .text { TextMessage(isMe: isMe, text: message.text ?? "") }
+            
             if isMe { Avatar(icon: message.member.icon) } else { Spacer() }
         }
         .padding(.init(top: 8, leading: 12, bottom: 8, trailing: 12))
@@ -34,13 +36,21 @@ private struct TextMessage: View {
     let text: String
     
     var body: some View {
-        Text(text)
-            .padding()
-            .background(background)
+        HStack(alignment: .top, spacing: 0) {
+            if !isMe { Arrow(isMe: isMe) }
+            
+            Text(text)
+                .font(.system(size: 17))
+                .padding(.horizontal, 12)
+                .padding(.vertical, 10)
+                .background(background)
+            
+            if isMe { Arrow(isMe: isMe) }
+        }
     }
     
     private var background: some View {
-        RoundedRectangle(cornerRadius: 3)
+        RoundedRectangle(cornerRadius: 4)
             .foregroundColor(Color("chat_\(isMe ? "me" : "friend")_background"))
     }
 }
@@ -52,5 +62,20 @@ private struct Avatar: View {
         Image(icon)
             .resizable()
             .frame(width: 40, height: 40)
+    }
+}
+
+private struct Arrow: View {
+    let isMe: Bool
+    
+    var body: some View {
+        Path { path in
+            path.move(to: .init(x: isMe ? 0 : 6, y: 14))
+            path.addLine(to: .init(x: isMe ? 0 : 6, y: 26))
+            path.addLine(to: .init(x: isMe ? 6 : 0, y: 20))
+            path.addLine(to: .init(x: isMe ? 0 : 6, y: 14))
+        }
+        .fill(Color("chat_\(isMe ? "me" : "friend")_background"))
+        .frame(width: 6, height: 30)
     }
 }
--- SwiftUI-WeChat/Module/Chat/ChatView.swift
+++ SwiftUI-WeChat/Module/Chat/ChatView.swift
@@ -15,6 +15,8 @@ struct ChatView: View {
     var body: some View {
         GeometryReader { proxy in
             VStack(spacing: 0) {
+                Separator(color: Color("navigation_separator"))
+                
                 List {
                     Group {
                         ForEach(self.messages) { message in
@@ -28,12 +30,7 @@ struct ChatView: View {
                     .listRowInsets(.zero)
                 }
                 
-                VStack(spacing: 0) {
-                    Color("chat_send_line")
-                        .frame(height: 1)
-                    Color("chat_send_background")
-                        .frame(height: proxy.safeAreaInsets.bottom + 56)
-                }
+                Send(proxy: proxy)
             }
         }
         .background(Color("light_gray"))
@@ -49,3 +46,38 @@ struct ChatView_Previews: PreviewProvider {
     }
 }
 #endif
+
+private struct Send: View {
+    let proxy: GeometryProxy
+    
+    @State var text: String = ""
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Separator(color: Color("chat_send_line"))
+            
+            ZStack {
+                Color("chat_send_background")
+                
+                VStack {
+                    HStack(spacing: 12) {
+                        Image("chat_send_voice")
+                        
+                        TextField("", text: $text)
+                            .frame(height: 40)
+                            .background(Color("chat_send_text_background"))
+                            .cornerRadius(4)
+                        
+                        Image("chat_send_emoji")
+                        Image("chat_send_more")
+                    }
+                    .frame(height: 56)
+                    .padding(.horizontal, 12)
+                    
+                    Spacer()
+                }
+            }
+            .frame(height: proxy.safeAreaInsets.bottom + 56)
+        }
+    }
+}
